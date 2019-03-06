d60ebcc0ada148ff8a3aee7cfe438b106655692e
Lutfur Rahaman <lutfur.rahaman@tcs.com>
2019-03-06 18:24:35 +0530

    US122 : SmsController.java file created

---
+++ com/app/notification/controller/SmsController.java
@@ -0,0 +1,8 @@
+public class SmsController {
+
+    public static void main(String[] args) {
+        // Prints "This is SmsController" to the terminal window.
+        System.out.println("This is SmsController");
+    }
+
+}
